5654a97bacba68222fb9a85d644ab837baf94caf
Владислав <metaluga1995@gmail.com>
2016-07-07 00:02:27 +0300

    Добавьте файлы проекта.

---
+++ TestTask1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myImg = new int[5, 5];
+            int k=1;
+            for(int i=0; i<5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    myImg[i, j] = k++;
+            }
+
+            Console.WriteLine("initial array");
+            foreach (int i in myImg)
+                Console.Write(i + " ");
+
+            Console.WriteLine("\n\narray after first algorithm");
+            foreach (int i in Rotation.rotates_imagesM(myImg)) // algorithm with limited memory
+                Console.Write(i + " ");
+
+            Console.WriteLine("\n\narray after second algorithm");
+
+            foreach (int i in Rotation.rotates_imagesF(myImg)) // algorithm with limited time
+                Console.Write(i + " ");
+
+        }
+    }
+}
---
+++ TestTask1/Rotation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask1
+{
+    class Rotation
+    {
+        public static int[,] rotates_imagesM(int[,] img) //limited memory
+        {
+            int scale = img.GetUpperBound(0) + 1;
+            
+            for (int i = 0; i <= scale / 2; i++)
+            {
+                for (int j = i; j <scale -i -1; j++)
+                {
+                    int tmp = img[scale - 1 - j, i]; 
+                    img[scale - 1 - j, i] = img[scale - 1 - i, scale - 1 - j];
+                    img[scale - 1 - i, scale - 1 - j] = img[j, scale - 1 - i];
+                    img[j, scale - 1 - i] = img[i, j];
+                    img[i, j] = tmp;
+                }
+            }
+            return img;
+        }
+
+        public static int[,] rotates_imagesF(int[,] img) //limited time
+        {
+            int scale = img.GetUpperBound(0) + 1;
+            int[,] outArr = new int[scale, scale];
+
+            for(int i = 0; i< scale; i++)
+                for (int j = 0; j< scale; j++)
+                    outArr[i, j] = img[scale - j - 1, i];
+
+
+            return outArr;
+        }
+
+    }
+}
